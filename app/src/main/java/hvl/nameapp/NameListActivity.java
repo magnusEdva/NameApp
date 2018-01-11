3a84da9fb96f4b35e69aba0fee73a466520370f2
magnusEdva <mlyngsedva@gmail.com>
2018-01-11 19:04:19 +0100

    added some image functionality to name list
    Signed-off-by: magnusEdva <mlyngsedva@gmail.com>

--- app/src/main/java/hvl/nameapp/NameListActivity.java
+++ app/src/main/java/hvl/nameapp/NameListActivity.java
@@ -1,13 +1,19 @@
 package hvl.nameapp;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import org.w3c.dom.NameList;
@@ -16,6 +22,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.jar.Attributes;
 
 public class NameListActivity extends AppCompatActivity {
     HashSet<Button> buttons;
@@ -33,14 +40,17 @@ public class NameListActivity extends AppCompatActivity {
 
     public void createList(){
         grid = (GridView) findViewById(R.id.gridview);
-        adapter = new ArrayAdapter<String>(this,
+        adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, getAllNames() );
         grid.setAdapter(adapter);
         grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v,
                                     int position, long id) {
-                Toast.makeText(NameListActivity.this,
-                        "" + position, Toast.LENGTH_SHORT).show();
+                ImageView image = (ImageView) findViewById(R.id.imageView);
+                int iid = getResources().getIdentifier(students.get((int)id).getPicture(), "image", getPackageName());
+
+
+                image.setVisibility(View.VISIBLE);
             }
         });
 
