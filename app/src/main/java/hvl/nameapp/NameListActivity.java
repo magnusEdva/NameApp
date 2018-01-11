14c54ce6ab7d0112ae595beef20c97bdc8c23791
magnusEdva <mlyngsedva@gmail.com>
2018-01-11 14:04:43 +0100

    some code

--- app/src/main/java/hvl/nameapp/NameListActivity.java
+++ app/src/main/java/hvl/nameapp/NameListActivity.java
@@ -1,29 +1,44 @@
 package hvl.nameapp;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.GridView;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class NameListActivity extends AppCompatActivity {
-
+    Set<String> students;
+    HashSet<Button> buttons;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_name_list);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        students = getStudents();
+        buttons = new HashSet<Button>();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
+    public Set<String> getStudents(){
+        SharedPreferences sharedPref = getPreferences(MODE_PRIVATE);
+        Set<String> students = sharedPref.getStringSet("students", null);
+        return students;
+    }
+
+
+    public void createList(){
+        GridView grid = new GridView(this);
+
+        for(int i = 0; i < students.size(); i++){
+            buttons.add(new Button(this));
+
+        }
+    }
 }
