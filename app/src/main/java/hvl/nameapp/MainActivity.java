612d9bed52aeb0fbf8487bf1bbc0e3c3cc6e00ab
magnusEdva <mlyngsedva@gmail.com>
2018-01-11 12:50:32 +0100

    added activityes

--- app/src/main/java/hvl/nameapp/MainActivity.java
+++ app/src/main/java/hvl/nameapp/MainActivity.java
@@ -1,13 +1,35 @@
 package hvl.nameapp;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Set;
+import java.util.HashSet;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        dummyInitiate();
+
+
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void dummyInitiate(){
+        Student Magnus = new Student("Magnus Edvardsen", "noe");
+        Student Steffen = new Student("Steffen","kommer");
+        Student Kolbein = new Student("Kolbein Horeson Foldøy","bilde");
+
+        Set<String> students = new HashSet<String>();
+
+        SharedPreferences sharedPref = getPreferences(MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putStringSet("students", students);
+
+        editor.commit();
     }
 }
