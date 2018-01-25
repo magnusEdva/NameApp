e8b9e334c8fa6b321d4946e51f3e4c2ace8b9bb4
magnusEdva <mlyngsedva@gmail.com>
2018-01-25 10:44:01 +0100

    added intent in NameApp

--- app/src/main/java/hvl/nameapp/NameApp.java
+++ app/src/main/java/hvl/nameapp/NameApp.java
@@ -1,6 +1,8 @@
 package hvl.nameapp;
 
 import android.app.Application;
+import android.content.Intent;
+import android.content.SharedPreferences;
 
 import java.util.ArrayList;
 
@@ -19,6 +21,18 @@ public class NameApp extends Application {
         PersonDataModel.dir = this.getFilesDir().getPath();
 
         persons = new PersonManager(this);
+
+
+        SharedPreferences sr = getSharedPreferences("preferences",MODE_PRIVATE);
+
+        String owner = sr.getString("owner", null);
+
+        if(owner == null){
+            Intent intent = new Intent(this, AddNewStudent.class);
+            intent.putExtra("makeOwner","new Owner");
+            startActivity(intent);
+        }
+
     }
 
     public ArrayList<PersonDataModel> getStudents() {
